60af29efb7287b98d4e8c64860414d2919a78644
CHELLYWadii <chellywadi3@gmail.com>
2021-11-15 23:38:47 +0100

    fix: add HasInvalidOrder

--- kata-coffee-machine/kata-coffee-machine/Customer.cs
+++ kata-coffee-machine/kata-coffee-machine/Customer.cs
@@ -45,9 +45,18 @@ namespace kata_coffee_machine
             }
         }
 
+        private bool HasInvalidOrder()
+        {
+            if (order.sugar > 2)
+                return true;
+            if (order.drink == DrinkType.NONE)
+                return true;
+            return false;
+        }
+
         public string MakeDrinks()
         {
-            if (order.sugar > 2 || order.drink == DrinkType.NONE)
+            if (HasInvalidOrder())
                 return "M:INVALID ORDER";
             string sugarCode, stickCode;
             CheckSugar(order.sugar, out sugarCode,out stickCode);
